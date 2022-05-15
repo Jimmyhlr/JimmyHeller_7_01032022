261ae784dcef25c850a5e163ac7b1cc203d837c5
Jimmyhlr <heller.jimmy.ci@gmail.com>
2022-05-15 09:10:12 +0200

    Fixing error messages display on signup form

--- back/routes/user.js
+++ back/routes/user.js
@@ -3,10 +3,9 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
 
 
-router.post('/signup', auth.validateRegister, userCtrl.signup, userCtrl.login);
+router.post('/signup', userCtrl.signup, userCtrl.login);
 router.post('/login', userCtrl.login);
 router.get('/userinfos', auth.isLoggedIn, userCtrl.userinfos);
 router.post('/deleteAccount', auth.isLoggedIn, userCtrl.deleteAccount);
